1aa3a476d810143039253b4126147a5c490e2c96
pfnazhmi <pfnazhmi@gmail.com>
2024-01-16 12:26:22 +0700

    init commit

---
+++ lib/core/config/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primary = Color(0xFFC0D72F);
+  static const textPrimary = Color(0xFF2E354C);
+  static const textSecondary = Color(0xFF7B8CB5);
+  static const success = Color(0xFF12B76A);
+  static const danger = Color(0xFFF14E42);
+}
